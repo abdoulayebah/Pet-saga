6f59c6be44b6afaff53e8043e2e399f9952dec7d
abdoulayebah <abdoulayewansan@yahoo.fr>
2015-07-19 17:22:17 +0200

    Update CubeVide.java

--- Bureau/code-source/CubeVide.java
+++ Bureau/code-source/CubeVide.java
@@ -5,9 +5,7 @@ import java.awt.Color;
  * <b> CubeVide est une classe héritant de la classe Cube et représentant un
  * cube vide.</b>
  *
- * @author tghaou28
  * @author abdbah39
- * @author sgasan32
  */
 public class CubeVide extends Cube {
 
